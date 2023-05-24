4b8834244094639334e01cb7190f2e23d88f119a
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-05-24 18:05:28 +0300

    setting cell display

---
+++ Heroes-Kingfisher/Views/CollectionViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  CollectionViewCell.swift
+//  Heroes-Kingfisher
+//
+//  Created by MikhaiL on 24.05.2023.
+//
+
+import UIKit
+import Kingfisher
+
+final class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.layer.cornerRadius = 15
+        }
+    }
+    
+    func configure(with superhero: Superhero) {
+        mainLabel.text = superhero.name
+        let imageURL = URL(string: superhero.images.lg)
+        
+        let processor = DownsamplingImageProcessor(size: imageView.bounds.size)
+        imageView.kf.indicatorType = .activity
+        imageView.kf.setImage(
+            with: imageURL,
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(1)),
+                .cacheOriginalImage
+            ]
+        ) {
+            result in
+            switch result {
+                case .success(let value):
+                    print("Task done for: \(value.source.url?.lastPathComponent ?? "")")
+                case .failure(let error):
+                    print("Job failed: \(error.localizedDescription)")
+            }
+        }
+    }
+}
