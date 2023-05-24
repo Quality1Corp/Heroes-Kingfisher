62d549a0b1e375c870a0c7063f6903dd2c99ff96
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-05-24 18:24:23 +0300

    fix code

--- Heroes-Kingfisher/ViewControllers/SuperHeroesViewController.swift
+++ Heroes-Kingfisher/ViewControllers/SuperHeroesViewController.swift
@@ -12,17 +12,17 @@ final class SuperHeroesViewController: UICollectionViewController {
     
     private var superheroes: [Superhero] = []
     private let networkManager = NetworkManager.shared
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchSuperheroes()
     }
-
+    
     // MARK: UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         superheroes.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "superhero", for: indexPath) as! CollectionViewCell
         let superhero = superheroes[indexPath.row]
@@ -41,13 +41,12 @@ final class SuperHeroesViewController: UICollectionViewController {
     private func fetchSuperheroes() {
         networkManager.fetchData { result in
             switch result {
-            case .success(let superheroes):
-                self.superheroes = superheroes
-                self.collectionView.reloadData()
-            case .failure(let error):
-                print(error)
+                case .success(let superheroes):
+                    self.superheroes = superheroes
+                    self.collectionView.reloadData()
+                case .failure(let error):
+                    print(error)
             }
         }
     }
 }
-
